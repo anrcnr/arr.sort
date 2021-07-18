c6c08cc538962bef298a8dcd9ac924131ebce92c
anrcnr <87597123+anrcnr@users.noreply.github.com>
2021-07-18 09:28:36 +0200

    =
    :)

---
+++ sort-.js
@@ -0,0 +1,15 @@
+let sort = (x)=>{
+		let entries = Object.entries(x)
+		let i = entries.length - 1
+		let arr = []
+		while(i-->0) {
+			if(entries[i]>val[0]) {
+				arr.unshift(entries[i])
+				continue
+			} 
+			if(entries[i]<arr[arr.length])
+			if(entries[i]<arr[arr.length-1])
+			{
+				arr.push(entries[i])
+			}
+}
